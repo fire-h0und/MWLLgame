691cd90bdb291c6ec38adf3350cdb26c7dc6732e
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2021-04-02 22:35:09 +0200

    Update MWLLauncher.sh

--- MWLLauncher.sh
+++ MWLLauncher.sh
@@ -4,8 +4,8 @@
 killall Crysis.exe
 killall -9 Crysis.exe
 
-cd "/home/cest/.wine.mwll/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis Wars/Mods/MWLL"
+cd "/home/cest/.wine/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis Wars/Mods/MWLL"
 pwd
 
-WINEPREFIX="/home/cest/.wine.mwll" wine64 "C:\Program Files (x86)\Electronic Arts\Crytek\Crysis Wars\Mods\MWLL\MWLLLauncher.exe"
+WINEPREFIX="/home/cest/.wine" wine64 "C:\Program Files (x86)\Electronic Arts\Crytek\Crysis Wars\Mods\MWLL\MWLLLauncher.exe"
 
