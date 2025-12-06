867bcf249626fbf3380f2fbd8e7c4e514e316e70
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2025-12-06 13:43:15 +0100

    Update MWLLauncher.sh
    Updated

--- MWLLauncher.sh
+++ MWLLauncher.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
-
+v="0.10-beta"
 
 killall Crysis.exe
 killall -9 Crysis.exe
 
-cd "/home/cest/.wine/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis Wars/Mods/MWLL"
+export WINEARCH=win64
+export WINEDEBUG=-all
+export WINEESYNC=1 #old tweak
+export WINEFSYNC=1 #new tweak
+
+# Vulkan support:
+DXVK_CONFIG_FILE=$HOME/.config/dxvk.conf
+
+export DXVK_HUD=scale=version,devinfo,fps,0.75  # ,gpuload,memory,frametimes
+
+cd "${HOME}/.wine/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis Wars/Mods/MWLL"
 pwd
 
-WINEPREFIX="/home/cest/.wine" wine64 "C:\Program Files (x86)\Electronic Arts\Crytek\Crysis Wars\Mods\MWLL\MWLLLauncher.exe"
-
+WINEPREFIX="${HOME}/.wine" wine64 "C:\Program Files (x86)\Electronic Arts\Crytek\Crysis Wars\Mods\MWLL\MWLLLauncher.exe"
