7407a5fa1bdba565a6abb58aaa5a86f36b9d77a2
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2025-12-06 13:44:38 +0100

    Update MWLLgame.sh

--- MWLLgame.sh
+++ MWLLgame.sh
@@ -1,65 +1,54 @@
 #! /bin/bash
 
-v=0.9-beta
+v="0.11-beta"
 executable=Crysis.exe
 shaderspath=~/Documents/My\ Games/Crysis\ Wars/Shaders
 
-# switch comment whats appropriate
-#  export WINEARCH=win32
-#  winegamepath='/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis Wars/Bin32'
-#  windowsgamepath='C:\Program Files (x86)\Electronic Arts\Crytek\Crysis Wars\Bin32\'
-
-  export WINEARCH=win64
+#we controll which directory we want be runned by the script 
   winegamepath='/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis Wars/Bin64'
   windowsgamepath='C:\Program Files (x86)\Electronic Arts\Crytek\Crysis Wars\Bin64\'
 
-#here we do "magic" to run DX9 (only supported so far)
+#here we do "magic" to run DX9
 #and tell Crysis we want the MWLL mod not the OEM game :^)
 OPT="-dx9 -mod MWLL"
 #OPT="-mod MWLL"
 
 # reducing what wine spews at us
 export WINEDEBUG=-all
-export WINEESYNC=1
+export WINEESYNC=1 #old tweak
+export WINEFSYNC=1 #new tweak
 
-# we controll which directory we want be runned by the script 
-  export WINEPREFIX=~/.wine
+# Vulkacn support:
+DXVK_CONFIG_FILE=$HOME/.config/dxvk.conf
+
+export DXVK_HUD=scale=version,devinfo,fps,0.75  # ,gpuload,memory,frametimes
+
+# set this accordingly
+# export DXVK_FILTER_DEVICE_NAME="NVIDIA GeForce GTX 1060 3GB"
 
 #this can really beak things if enabled but try it?
 #  export MESA_DEBUG=1
-
 
 # we the humans check here what renders our stuff
 # if not "Gallium" then it's software = too slow
 REND=$(glxinfo | grep "OpenGL renderer" | awk -F: '{print $2}' | awk -F"(" '{print $1}')
 echo "Renderer:"$REND
-echo "if the above is llvm and not an actual card the game will not work"
+echo "if the above is llvm and not an actual card the game may render too slow"
 
 #cleaning leftovers from late dinner ;)
 killall $executable
 killall -9 $executable
 
-#cleaning shader cache for better performance each time
+#pro-tip: cleaning shader cache for better performance each time
 rm -r "${shaderspath}/*"
 
-#here I cleverly avoid calling the path with my user name :)
 cd "${WINEPREFIX}${winegamepath}/${binpath}"
-#here i cleverly ask to show the path so we see what WINEPREFIX was called
 pwd
 
-case $WINEARCH in
-win64)
-     echo 64bit
-     wine64 "${windowsgamepath}${executable}" $OPT # &> MWLL_debug.log
-     #if uncommented, the last line's comment dumps a log file to the game folder - might prove usefull or a vaste of disk space
-     ;;
-win32)
-     echo 32bit
-     #here (hopefully) no shit hits the fan, and we land in game menu
-     wine ${windowsgamepath}${executable} $OPT # &> MWLL_debug.log
-     #if uncommented, the last line's comment dumps a log file to the game folder - might prove usefull or a vaste of disk space
-     ;;
-esac
-# to restore resolution put here what's appropriate or leave as is and just uncomment
-xrandr -s 0 # last good resolution
+WINEPREFIX="${HOME}/.wine" wine64 "${windowsgamepath}${executable}" $OPT #2>/dev/null # &> MWLL_debug.log
+
+# this should work for most single monitor setups, adjust accordingly
+xrandr --output $(xrandr | grep " connected" | awk '{print $1}') -s 0
+# alternative version
+#xrandr --output $(xrandr | grep " connected" | awk '{print $1}') --mode 1680x1050
 
